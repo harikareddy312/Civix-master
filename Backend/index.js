8dd8abf966375c2175b4eb4f555e06f18f6f7ad6
harika parmati <parmatiharika@gmail.com>
2026-04-08 23:10:37 +0530

    changed backend

--- Backend/index.js
+++ Backend/index.js
@@ -27,8 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "civix-eta.vercel.app",
-  // http://localhost:5173
+  origin: "https://civix-master.vercel.app",
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
